6089b36a9a001889d94c43c9fa7e91a87998ea9d
FriendswoodCodeNinjas <93798411+FriendswoodCodeNinjas@users.noreply.github.com>
2021-11-24 18:23:22 -0600

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,7 +7,7 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Console.log();
+        
     }
 
     // Update is called once per frame
